5437aa3d3c7b4d7de1eece3382531e6168c06dfd
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:57:54 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,14 +1,14 @@
 
 <div style="width:100%">
 <div style="margin-right:6px;width:120px" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Prepress</div>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i> CallTag</span></button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span><br> Create</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Produce</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Finish</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Fabricate</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Quality</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Fullfill</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Install</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Overview</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Calendar</button>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i> CallTag</span></div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Create</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Produce</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Finish</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Fabricate</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Quality</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Fullfill</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Install</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Overview</div>
+<div style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Calendar</div>
 </div>

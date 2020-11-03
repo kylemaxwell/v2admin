8521a739728ac1c4dfa387cb332ca35ef0b58b16
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:49:59 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,8 +1,8 @@
 
 <div style="display:flex;justify-content:flex-start;">
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Prepress</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Call Tags</button>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Create</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span><br> Prepress</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span><br> Call Tags</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span><br> Create</button>
 <button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Produce</button>
 <button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Finish</button>
 <button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Fabricate</button>

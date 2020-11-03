a4ef22da5d8fa70e6ca83b014db80e4bb8747f6a
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:32:32 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,10 +1,10 @@
 
 <div style="display:flex;justify-content:flex-start;">
-<button style="padding-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
 </div>

25e8e0a6af41bd3fcd9c06c43b9bd596ebc8451e
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:31:02 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,6 +1,6 @@
 
-<div style="display:flex;justify-content:flex-start;padding:5px">
-<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+<div style="display:flex;justify-content:flex-start;">
+<button style="padding-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
 <button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
 <button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
 <button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>

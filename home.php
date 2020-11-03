92b1c9a07ecb8e19df0047aedb3abf7f3d123885
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:03:27 -0700

    1

--- home.php
+++ home.php
@@ -315,7 +315,7 @@
                 <!-- ============================================================== -->
                 <div class="row page-titles">
                     <div class="col-md-5 align-self-center">
-                        <h4 class="text-themecolor">Dashboard 1</h4>
+                        <?php include "buttonmenu.php"; ?>
                     </div>
                     <div class="col-md-7 align-self-center text-right">
                         <div class="d-flex justify-content-end align-items-center">

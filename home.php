be9bbeb004aa771d89cf498f8da069e96fa3e47c
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 15:32:12 -0700

    1

--- home.php
+++ home.php
@@ -297,38 +297,7 @@
         <!-- ============================================================== -->
         <!-- Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->
-        <aside class="left-sidebar">
-            <!-- Sidebar scroll-->
-            <div class="scroll-sidebar">
-                <!-- Sidebar navigation-->
-                <nav class="sidebar-nav">
-                    <ul id="sidebarnav">
-
-                        <li> <a class="waves-effect waves-dark" href="home.php?id=categories"><i class="icon-list"></i><span class="hide-menu">Categories</span> <span class="badge badge-pill badge-cyan ml-auto">4</span></a></li>
-
-                        <li> <a class="waves-effect waves-dark" href="home.php?id=products"><i class="ti-shopping-cart"></i><span class="hide-menu">Products</span> <span class="badge badge-pill badge-cyan ml-auto">24</span></a></li>
-
-                        <li> <a class="waves-effect waves-dark" href="home.php?id=options" aria-expanded="false"><i class="ti-settings"></i><span class="hide-menu">Options</span></a>
-                        <li> <a class="waves-effect waves-dark" href="home.php?id=vieworders" aria-expanded="false"><i class="ti-settings"></i><span class="hide-menu">Orders</span></a>
-
-                        </li>
-
-                        <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class=" ti-layers-alt"></i><span class="hide-menu">Option Groups</span></a>
-                            <ul aria-expanded="false" class="collapse">
-                              <li><a href="index.php?id=products"> View Option Groups </a></li>
-                              <li><a href="index2.html">New Group</a></li>
-                            </ul>
-                        </li>
-
-
-                        <li> <a class="waves-effect waves-dark" href="../documentation/documentation.html" aria-expanded="false"><i class="far fa-circle text-danger"></i><span class="hide-menu">Documentation</span></a></li>
-                        <li> <a class="waves-effect waves-dark" href="pages-login.html" aria-expanded="false"><i class="far fa-circle text-success"></i><span class="hide-menu">Log Out</span></a></li>
-                    </ul>
-                </nav>
-                <!-- End Sidebar navigation -->
-            </div>
-            <!-- End Sidebar scroll-->
-        </aside>
+<?php include "sidebar.php"; ?>
         <!-- ============================================================== -->
         <!-- End Left Sidebar - style you can find in sidebar.scss  -->
         <!-- ============================================================== -->

0ae739cf89b9b8780ebf6356bf36d41d6b6d9ecb
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-02 17:13:02 -0700

    1

---
+++ view_orders.php
@@ -0,0 +1,46 @@
+
+
+<div style="width:100%;display:flex;justify-content:space-between">
+
+  <div style="width:49%;border:1px solid blue">
+    1
+  </div>
+
+
+  <div style="width:49%;">
+    <div style="width:100%;display:flex;justify-content:flex-start;flex-wrap:wrap;align-items:flex-start;">
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Edit Order</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Re-Order</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Stickies</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Re-Print</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm"><span class="btn-label"><i class="fa fa-envelope-o"></i></span>Proof</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Payment</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Templates</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Work Order</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Delivery</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Invoice</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Reciept</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Job Ticket</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">CC#</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">DL Art</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">DL Files</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Add Part</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Small Label</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Big Label</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">Split Inv/Est</button>
+      <button style="width:110px;margin:0px 5px 5px 0px;" class="btn btn-info waves-lightbtn-sm">GBU</button>
+    </div>
+<br>
+<h4>ORDER #[Number][Child]</h4><br>
+Taken:<br>
+Approved:<br>
+Production Due Date:<br>
+Estimated In-Hand Date:<br>
+Child Account:<br>
+Store Location:<br>
+Production Location:<br>
+Order Tag::<br>
+
+  </div>
+
+</div>

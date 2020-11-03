be444f96c32f287fcf4afff9b174a523d01ed69f
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:55:54 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,7 +1,7 @@
 
-<div style="display:flex;justify-content:flex-start;">
+<div style="width:100%">
 <div style="margin-right:6px;width:120px" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Prepress</div>
-<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i> Call Tag</span></button>
+<button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i> CallTag</span></button>
 <button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span><br> Create</button>
 <button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Produce</button>
 <button style="margin-right:6px;" class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span> Finish</button>

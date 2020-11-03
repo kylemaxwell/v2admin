6aca6af63513429eb7961e2bd6225fe26d9a1bc2
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 16:24:24 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,1 +1,5 @@
-Queue buttons
+
+<div class="buttonmenu">
+<button class="btn btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-envelope"></i></span>Mail</button>
+
+</div>

f67a4f70aec507aa926c94ecd1e4d51bcd49af15
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 11:09:23 -0700

    Update reports.php

--- reports.php
+++ reports.php
@@ -4,10 +4,9 @@
        value="2018-07-22"
        min="2018-01-01" max="2018-12-31">
  to
-
  <input type="date" id="start" name="trip-start"
-        value="2018-07-22"
-        min="2018-01-01" max="2018-12-31">
+        value="2020-07-22"
+        min="2018-01-01" max="2020-12-31">
 
 <button class="btn btn-info">Run</button>
 <button class="btn btn-info">Schedule</button>

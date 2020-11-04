a851378da95b079b31358112794b1503c49d5704
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 11:06:15 -0700

    Update reports.php

--- reports.php
+++ reports.php
@@ -1,1 +1,10 @@
-Reports go here.
+<label for="start">Start date:</label>
+
+<input type="date" id="start" name="trip-start"
+       value="2018-07-22"
+       min="2018-01-01" max="2018-12-31">
+ to
+
+ <input type="date" id="start" name="trip-start"
+        value="2018-07-22"
+        min="2018-01-01" max="2018-12-31">

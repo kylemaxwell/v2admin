388a9e59695c728ccdd65928fba2b3bce7855dd2
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 16:31:41 -0700

    Update reports.php

--- reports.php
+++ reports.php
@@ -25,11 +25,11 @@ reports v1.6
 
 <form action="/action_page.php">
 <div style="display:flex;justify-content:space-between;">
-<div><input type="checkbox" id="order#" name="order#" value="order#"><label style="padding:5px" for="order#"> Order #</label></div>
-<div><input type="checkbox" id="client" name="client" value="client"><label for="client"> Client</label></div>
-<div><input type="checkbox" id="salestax" name="salestax" value="salestax"><label for="salestax"> Sales Tax</label></div>
-<div><input type="checkbox" id="orderdesc" name="orderdesc" value="orderdesc"><label for="orderdesc"> Order Description</label></div>
-<div><input type="checkbox" id="cost" name="cost" value="cost"><label for="cost"> Cost</label></div>
+<div><input type="checkbox" id="order#" name="order#" value="order#"><label style="padding-left:5px" for="order#"> Order #</label></div>
+<div><input type="checkbox" id="client" name="client" value="client"><label style="padding-left:5px" for="client"> Client</label></div>
+<div><input type="checkbox" id="salestax" name="salestax" value="salestax"><label style="padding-left:5px" for="salestax"> Sales Tax</label></div>
+<div><input type="checkbox" id="orderdesc" name="orderdesc" value="orderdesc"><label style="padding-left:5px" for="orderdesc"> Order Description</label></div>
+<div><input type="checkbox" id="cost" name="cost" value="cost"><label style="padding-left:5px" for="cost"> Cost</label></div>
 <br><br>
 </div>
 </form>

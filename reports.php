f3d9b39e400d440ba5381f1d45c392c531bac2b0
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 11:25:43 -0700

    Update reports.php

--- reports.php
+++ reports.php
@@ -1,14 +1,18 @@
-<label for="start">Start date:</label>
+<div style="display:flex;justify-content:flex-start;">
 
-<input type="date" id="start" name="trip-start"
+<label for="start">Start Date:</label>
+
+<input style="align-self:flex-start" type="date" id="start" name="trip-start"
        value="2018-07-22"
        min="2018-01-01" max="2018-12-31">
  to
- <input type="date" id="start" name="trip-start"
+ <input style="align-self:flex-start" type="date" id="start" name="trip-start"
         value="2020-07-22"
         min="2018-01-01" max="2020-12-31">
 
-<button class="btn btn-sm btn-success">Run</button>
-<button class="btn btn-sm btn-success">Schedule</button>
-<button class="btn btn-sm btn-success">Create PDF</button>
-<button class="btn btn-sm btn-success">Save Script</button>
+<button style="align-self:flex-end" class="btn btn-sm btn-success">Run</button>
+<button style="align-self:flex-end" class="btn btn-sm btn-success">Schedule</button>
+<button style="align-self:flex-end" class="btn btn-sm btn-success">Create PDF</button>
+<button style="align-self:flex-end" class="btn btn-sm btn-success">Save Script</button>
+
+</div>

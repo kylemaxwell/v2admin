ddd2636f52b7c1d5cf95a75c26c2e70543735f22
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 11:35:59 -0700

    Update reports.php

--- reports.php
+++ reports.php
@@ -1,21 +1,21 @@
-reports v1.0
+reports v1.1
 
-<div style="display:flex;">
+<div style="width:100%">
 
 <label for="start">Start Date:</label>
 
-<input style="align-self:flex-start" type="date" id="start" name="trip-start"
+<input type="date" id="start" name="trip-start"
        value="2018-07-22"
        min="2018-01-01" max="2018-12-31">
  to
- <input style="align-self:flex-start" type="date" id="start" name="trip-start"
+ <input type="date" id="start" name="trip-start"
         value="2020-07-22"
         min="2018-01-01" max="2020-12-31">
 
-<div type="button" style="align-self:flex-end" class="btn btn-sm btn-success">Run</div>
-<button style="align-self:flex-end" class="btn btn-sm btn-success">Schedule</button>
-<button style="align-self:flex-end" class="btn btn-sm btn-success">Create PDF</button>
-<button style="align-self:flex-end" class="btn btn-sm btn-success">Save Script</button>
+<button class="btn btn-sm btn-success">Run</button>
+<button class="btn btn-sm btn-success">Schedule</button>
+<button class="btn btn-sm btn-success">Create PDF</button>
+<button class="btn btn-sm btn-success">Save Script</button>
 
 </div>
 
@@ -23,6 +23,7 @@ reports v1.0
   <thead>
     <tr>
       <th>
+
       </th>
     </tr>
   <thead>

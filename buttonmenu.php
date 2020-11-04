caab0750a0a9949e5ce9d990f770ee29590a3d19
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 17:39:30 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -8,7 +8,7 @@
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far ti-pin-alt"></i></span> Fabricate</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-hourglass-end"></i></span> Quality</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-box"></i></span> Fullfill</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far f006mdi-account-box"></i></span> Install</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far mdi-account-box"></i></span> Install</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-eye"></i></span> Overview</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-calendar"></i></span> Calendar</div>
 </div>

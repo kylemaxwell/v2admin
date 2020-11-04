c9ec43ec015bf3c55f31af269583a2d15b28dfb2
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 15:44:49 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -1,14 +1,14 @@
 
 <div style="width:100%">
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder" class="far icon-badge"></i></span> Prepress</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far icon-phone"></i></span> CallTag</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-seedling"></i></span> Create</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-hourglass-half"></i></span> Produce</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-people-carry"></i></span> Finish</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far icon-pin"></i></span> Fabricate</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far icon-badge"></i></span> Quality</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-box"></i></span> Fullfill</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far icon-wrench"></i></span> Install</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-eye"></i></span> Overview</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-calendar"></i></span> Calendar</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far icon-phone"></i></span> CallTag</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far fa-seedling"></i></span> Create</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far fa-hourglass-half"></i></span> Produce</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far fa-people-carry"></i></span> Finish</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far icon-pin"></i></span> Fabricate</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far icon-badge"></i></span> Quality</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far fa-box"></i></span> Fullfill</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far icon-wrench"></i></span> Install</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far fa-eye"></i></span> Overview</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i style="font-weight:bolder"  class="far fa-calendar"></i></span> Calendar</div>
 </div>

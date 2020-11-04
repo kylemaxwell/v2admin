9ac17a0f3351120b1acd315a7088ab391e9cd377
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-03 17:37:31 -0700

    Update buttonmenu.php

--- buttonmenu.php
+++ buttonmenu.php
@@ -5,7 +5,7 @@
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-seedling"></i></span> Create</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-hourglass-half"></i></span> Produce</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-people-carry"></i></span> Finish</div>
-<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-wrench"></i></span> Fabricate</div>
+<div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far ti-pin-alt"></i></span> Fabricate</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-hourglass-end"></i></span> Quality</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-box"></i></span> Fullfill</div>
 <div style="margin-right:6px;" class="btn btn-sm btn-info waves-effect waves-light" type="button"><span class="btn-label"><i class="far fa-map-marker"></i></span> Install</div>

a3a40d693f57a82ab403cc93315d12582483ad08
Kyle Maxwell <kylemaxster@gmail.com>
2020-11-04 16:24:50 -0700

    Update reports.php

--- reports.php
+++ reports.php
@@ -12,20 +12,26 @@ reports v1.6
         value="2020-07-22"
         min="2018-01-01" max="2020-12-31">
 
+
 <button class="btn btn-sm btn-success">Run</button>
 <button class="btn btn-sm btn-success">Schedule</button>
 <button class="btn btn-sm btn-success">Create PDF</button>
 <button class="btn btn-sm btn-success">Save Script</button>
 
+
 </div>
 <br><br>
+
+
 <form action="/action_page.php">
+<div style="display:flex;justify-content:flex-start;">
 <input type="checkbox" id="order#" name="order#" value="order#">
 <label for="order#"> Order #</label><br>
 <input type="checkbox" id="client" name="client" value="client">
 <label for="client"> Client</label><br>
 <input type="checkbox" id="salestax" name="salestax" value="salestax">
 <label for="salestax"> Sales Tax</label><br><br>
+</div>
 </form>
 <br><br>
 <table id="#example" style="width:100%" class="table display no-wrap table-hover table-condensed table-bordered table-striped">
